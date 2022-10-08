3586fcff2988c6f07d9e27e70ef8682de2323482
denis <denis.timoshitskiy@gmail.com>
2022-10-08 16:49:48 +0300

    Button search and readme.md

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	// Открытие окна поиска пациента
+
+	const formSearch = document.querySelector('.form-search');
+	const inputSearch = document.querySelector('.form-search__input');
+
+	formSearch.addEventListener('mouseenter', () => {
+		inputSearch.classList.add('form-search__input--visually');
+	});
+	formSearch.addEventListener('mouseleave', () => {
+		inputSearch.classList.remove('form-search__input--visually');
+	});
 
 })
