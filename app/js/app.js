31a6910ec3184b3a59a81842ac008a6bdf90ad61
denis <denis.timoshitskiy@gmail.com>
2022-10-08 22:25:35 +0300

    Form add doctor

--- app/js/app.js
+++ app/js/app.js
@@ -5,11 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
 	const formSearch = document.querySelector('.form-search');
 	const inputSearch = document.querySelector('.form-search__input');
 
-	formSearch.addEventListener('mouseenter', () => {
-		inputSearch.classList.add('form-search__input--visually');
-	});
-	formSearch.addEventListener('mouseleave', () => {
-		inputSearch.classList.remove('form-search__input--visually');
-	});
+	if (formSearch) {
+		formSearch.addEventListener('mouseenter', () => {
+			inputSearch.classList.add('form-search__input--visually');
+		});
+		formSearch.addEventListener('mouseleave', () => {
+			inputSearch.classList.remove('form-search__input--visually');
+		});
+	}
 
 })
